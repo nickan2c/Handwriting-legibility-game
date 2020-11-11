acb567f9fb2c9b59c7bea00de035e1b9b1db525a
nickan2c <nickan2c@gmail.com>
2020-11-11 20:17:12 +0000

    login and register added

--- tkinter_window.py
+++ tkinter_window.py
@@ -1,61 +1,58 @@
 from tkinter import *
 
+
 def main_account_screen():
     global main_screen
 
-    # add command=register in button widget
-
-    main_screen = Tk()   # create window
-    main_screen.geometry("300x200") # set window size
-    main_screen.title("Account Login") # set window title
+    main_screen = Tk()  # create window
+    main_screen.geometry("350x200")  # set window size
+    main_screen.title("Account Login")  # set window title
 
     # create a Form label
-    Label(text="Choose Login Or Register", bg="blue", width="300", height="2", font=("Calibri", 13)).pack()
-    Label(text="").pack()
+    Label(text="Welcome to the handwriting test!", bg="teal", width="300", height="2").pack()
+    Label(text="").pack()  # leaves a gap for aesthetic purposes
 
     # create Login Button
-    Button(text="Login", height="2", width="30").pack()
+    Button(text="Login", height="2", width="30", command=login).pack()  # height and width defined for aesthetics
     Label(text="").pack()
 
     # create a register button
-    Button(text="Register", height="2", width="30", command=register).pack()
-    main_screen.mainloop() # start the GUI
+    Button(text="Register", height="2", width="30", command=register).pack()  # upon clicking, calls register function
+    main_screen.mainloop()  # start the GUI
+
+
+def login():
+    global login_screen
+    login_screen = Toplevel(main_screen)
+    login_screen.title("Login")
+    login_screen.geometry("300x250")
+    Label(login_screen, text="Please enter details below to login").pack()
+    Label(login_screen, text="").pack()
+
+    global verify_username
+    global verify_password
+
+    username = StringVar()  # defines how the text entered will be stored
+    password = StringVar()
+
+    global username_login_entry
+    global password_login_entry
+
+    Label(login_screen, text="Username * ").pack()
+    username_login_entry = Entry(login_screen, textvariable=username)  # makes the text entered into a variable
+    username_login_entry.pack()
+
+    Label(login_screen, text="").pack()
+    Label(login_screen, text="Password * ").pack()
+
+    password_login_entry = Entry(login_screen, textvariable=password, show='*')
+    password_login_entry.pack()
+
+    Label(login_screen, text="").pack()
+    Button(login_screen, text="Login", width=10, height=1).pack()
 
 
 def register():
-    register_screen = Toplevel(main_screen)
-    register_screen.title("Register")
-    register_screen.geometry("300x250")
+    pass
 
-    # Set text variables
-    username = StringVar()
-    password = StringVar()
-
-    # Set label for user's instruction
-    Label(register_screen, text="Please enter details below", bg="blue").pack()
-    Label(register_screen, text="").pack()
-
-    # Set username label
-    username_lable = Label(register_screen, text="Username * ")
-    username_lable.pack()
-
-    # Set username entry
-    # The Entry widget is a standard Tkinter widget used to enter or display a single line of text.
-
-    username_entry = Entry(register_screen, textvariable=username)
-    username_entry.pack()
-
-    # Set password label
-    password_lable = Label(register_screen, text="Password * ")
-    password_lable.pack()
-
-    # Set password entry
-    password_entry = Entry(register_screen, textvariable=password, show='*')
-    password_entry.pack()
-
-    Label(register_screen, text="").pack()
-
-    # Set register button
-    Button(register_screen, text="Register", width=10, height=1, bg="blue").pack()
-
-main_account_screen() # call the main_account_screen() function
+main_account_screen()
