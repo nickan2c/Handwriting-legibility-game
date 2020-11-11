f9b2bed2e762a35bb45b6fb314297999e412ab16
nickan2c <nickan2c@gmail.com>
2020-11-11 21:02:23 +0000

    Decided to use class, get rid of globals.

--- tkinter_window.py
+++ tkinter_window.py
@@ -1,58 +1,57 @@
 from tkinter import *
 
 
-def main_account_screen():
-    global main_screen
+class logreg:
 
-    main_screen = Tk()  # create window
-    main_screen.geometry("350x200")  # set window size
-    main_screen.title("Account Login")  # set window title
+    def main_screen(self):
+        self.main_screen = Tk()  # create window
+        self.main_screen.geometry("300x250")  # set window size
+        self.main_screen.title("Account Login")  # set window title
 
-    # create a Form label
-    Label(text="Welcome to the handwriting test!", bg="teal", width="300", height="2").pack()
-    Label(text="").pack()  # leaves a gap for aesthetic purposes
+        Label(text="Please Select Your Choice", bg="white", width="20", height="2").pack()  # pack puts it into screen
+        Label(text="").pack()  # leaves a gap for aesthetic purposes
 
-    # create Login Button
-    Button(text="Login", height="2", width="30", command=login).pack()  # height and width defined for aesthetics
-    Label(text="").pack()
+        Button(text="Login", height="2", width="15", command=self.login).pack()  # upon clicking, calls login function
+        Label(text="").pack()
 
-    # create a register button
-    Button(text="Register", height="2", width="30", command=register).pack()  # upon clicking, calls register function
-    main_screen.mainloop()  # start the GUI
+        Button(text="Register", height="2", width="15",
+               command=self.register).pack()  # upon clicking, calls register function
+
+        self.main_screen.mainloop()
+
+    def register(self):
+        pass
+
+    def register_user(self):
+        pass
+
+    def login(self):
+        self.login_screen = Toplevel(self.main_screen)
+        self.login_screen.title("Login")
+        self.login_screen.geometry("300x250")
+
+        Label(self.login_screen, text="Please enter your details below").pack()
+        Label(self.login_screen, text="").pack()
+
+        # define how the text entered will be stored
+        self.username_to_verify = StringVar()
+        self.password_to_verify = StringVar()
+
+        Label(self.login_screen, text="Username * ").pack()
+
+        self.username_login_entry = Entry(self.login_screen, textvariable=self.username_to_verify).pack()
+
+        Label(self.login_screen, text="").pack()
+        Label(self.login_screen, text="Password * ").pack()
+
+        self.password_login_entry = Entry(self.login_screen, textvariable=self.password_to_verify, show='*').pack()
+
+        Label(self.login_screen, text="").pack()
+        Button(self.login_screen, text="Login", width=10, height=1, command=self.login_verify).pack()
+
+    def login_verify(self):
+        pass
 
 
-def login():
-    global login_screen
-    login_screen = Toplevel(main_screen)
-    login_screen.title("Login")
-    login_screen.geometry("300x250")
-    Label(login_screen, text="Please enter details below to login").pack()
-    Label(login_screen, text="").pack()
-
-    global verify_username
-    global verify_password
-
-    username = StringVar()  # defines how the text entered will be stored
-    password = StringVar()
-
-    global username_login_entry
-    global password_login_entry
-
-    Label(login_screen, text="Username * ").pack()
-    username_login_entry = Entry(login_screen, textvariable=username)  # makes the text entered into a variable
-    username_login_entry.pack()
-
-    Label(login_screen, text="").pack()
-    Label(login_screen, text="Password * ").pack()
-
-    password_login_entry = Entry(login_screen, textvariable=password, show='*')
-    password_login_entry.pack()
-
-    Label(login_screen, text="").pack()
-    Button(login_screen, text="Login", width=10, height=1).pack()
-
-
-def register():
-    pass
-
-main_account_screen()
+l = logreg()
+l.main_screen()
