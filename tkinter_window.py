68f373d89ffe5d22882232533d7f56c8171b5e4f
nickan2c <nickan2c@gmail.com>
2020-11-21 10:35:03 +0000

    grab login values inputted

--- tkinter_window.py
+++ tkinter_window.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import sqlite3
 
 
 class logreg:
@@ -31,13 +32,17 @@ class logreg:
         Label(self.register_screen, text="Please enter your details below").pack()
         Label(self.register_screen, text="").pack()
 
-        username_label = Label(self.register_screen, text="Username * ").pack()
+        username_label = Label(self.register_screen, text="Username * ")
+        username_label.pack()
 
-        self.username_entry = Entry(self.register_screen, textvariable=self.username).pack()
+        self.username_entry = Entry(self.register_screen, textvariable=self.username)
+        self.username_entry.pack()
 
-        password_label = Label(self.register_screen, text="Password * ").pack()
+        password_label = Label(self.register_screen, text="Password * ")
+        password_label.pack()
 
-        self.password_entry = Entry(self.register_screen, textvariable=self.password, show='*').pack()
+        self.password_entry = Entry(self.register_screen, textvariable=self.password, show='*')
+        self.password_entry.pack()
 
         Label(self.register_screen, text="").pack()
         Button(self.register_screen, text="Register", width=10, height=1, bg="blue", command=self.register_user).pack()
@@ -66,8 +71,13 @@ class logreg:
 
         self.password_login_entry = Entry(self.login_screen, textvariable=self.password_to_verify, show='*').pack()
 
-        Button(self.login_screen, text="Login", width=10, height=1, command=self.login_verify).pack()
+        def login_verify():
+            username = self.username_to_verify.get()
+            password = self.password_to_verify.get()
+            print(username, 'you have pressed submit')
+
         Label(self.login_screen, text="").pack()
+        Button(self.login_screen, text="Confirm Login", width=10, height=1, command=login_verify).pack()
 
     def login_verify(self):
         pass
