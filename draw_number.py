dcde1d6a64cb1cbca61db07c9d796fcb432ca5ce
nickan2c <nickan2c@gmail.com>
2020-11-30 20:21:42 +0000

    added time

--- draw_number.py
+++ draw_number.py
@@ -1,17 +1,19 @@
-# https://www.python-course.eu/tkinter_events_binds.php
+ https://www.python-course.eu/tkinter_events_binds.php
 
 from tkinter import *
 import PIL
-from PIL import Image, ImageDraw
-
+from PIL import Image, ImageGrab
+import time
 
 class drawing:
     def __init__(self, prompt):  # prompt is the number that the user should draw
-        root = Tk()  # initiate window
-        self.prompt = Label(text=prompt)
-        self.prompt.pack()
+        self.root = Tk()  # initiate window
+        Label(text=('draw a {}'.format(prompt))).pack()
         self.lastx, self.lasty = None, None  # at this point, user hasn't clicked yet so defined as none
-        self.canvas = Canvas(root, width=280, height=280)  # width and height based on the 28x28 required mnist size
+        self.canvas = Canvas(self.root, width=280, height=280)  # width and height based on the 28x28 required mnist size
+
+        self.seconds = 4
+        Label(text=('you have {} seconds'.format(seconds))).pack()
 
         self.canvas.bind('<Button-1>',
                          self.activate_paint)  # button 1 is mouse left click, binded so that every time mouse left click is
@@ -21,10 +23,16 @@ class drawing:
         submit_button = Button(text="submit", command=self.submit)
         submit_button.pack()
 
-        root.mainloop()
+        self.root.mainloop()
 
     def submit(self):
-        pass
+        x = self.root.winfo_rootx() + self.canvas.winfo_x()  # gets left x coordinate
+        y = self.root.winfo_rooty() + self.canvas.winfo_y() # gets top y coordinate
+        x1 = x + self.canvas.winfo_width()  # gets right side x coordinate by adding width to leftmost x coordinate
+        y1 = y + self.canvas.winfo_height()  # gets bottom y coordinate by adding height to top y coordinate
+        ImageGrab.grab().crop((x, y, x1, y1)).save('drawing.png') # imagegrab.grab 'screenshots' at those coordinates
+
+        print('image saved')
 
     def activate_paint(self, event):  # event is passed in from canvas.bind, where
         self.canvas.bind('<B1-Motion>', self.paint)  # when mouse is moved while left click is held, it paints
@@ -37,4 +45,9 @@ class drawing:
         self.lastx, self.lasty = self.x_co, self.y_co  # redefine last x last y as point where user clicked
 
 
-d = drawing()
+import random
+num_games = 0  # will be selected by user
+d = drawing(6)
+
+
+
