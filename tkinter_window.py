90bb7b00261aa3d36e34462ce0ec3f9175936768
nickan2c <63848807+nickan2c@users.noreply.github.com>
2020-12-10 20:20:40 +0000

    leaderboard

--- tkinter_window.py
+++ tkinter_window.py
@@ -36,14 +36,16 @@ class database():
           username text NOT NULL,
           pw_hashed text NOT NULL,
           salt  text    NOT NULL,
-          accuracy integer
+          accuracy integer,
           num_attempts integer
           )''')
 
         self.conn.commit()
         pw, s = hash_password('1')
+        self.c.execute("INSERT INTO HandwritingUsers(username,pw_hashed, salt, num_attempts) VALUES(?,?,?,?)",
+                       ('example', pw, s, 1))  # done in this way to prevent sql injection
         self.c.execute("INSERT INTO HandwritingUsers(username,pw_hashed, salt) VALUES(?,?,?)",
-                       ('example', pw, s))  # done in this way to prevent sql injection
+                       ('jane', pw, s))
         self.conn.commit()
 
     def insert(self, username, password):
@@ -72,30 +74,56 @@ class database():
             print('you"re in')
             return True
 
+    def display_leaderboard(self):
+        self.c.execute('SELECT username, accuracy, num_attempts FROM HandwritingUsers WHERE num_attempts != 0 ORDER BY accuracy ASC')
+        self.conn.commit()
+
+        return self.c.fetchall()
+
 
 class logreg:
 
     def __init__(self):
         self.db = database()
+        self.login_success = False
 
-    def main_screen(self):
-        self.main_screen = Tk()  # create window
-        self.main_screen.geometry("300x250")  # set window size
-        self.main_screen.title("Account Login")  # set window title
+    def _start_screen(self):
+        self.root = Tk()
+        self.root.geometry("300x250")  # set window size
+        self.root.title("Account Login")  # set window title
 
         Label(text="Please Select Your Choice", bg="white", width="20", height="2").pack()  # pack puts it into screen
         Label(text="").pack()  # leaves a gap for aesthetic purposes
 
-        Button(text="Login", height="2", width="15", command=self.__login).pack()  # upon clicking, calls login function
+        Button(text="Leaderboard", height="2", width="15",
+               command=self.__leaderboard).pack()  # upon clicking, calls login function
         Label(text="").pack()
 
-        Button(text="Register", height="2", width="15",
-               command=self.__register).pack()  # upon clicking, calls register function
+        Button(text="play", height="2", width="15",
+               command=self.__main_screen).pack()  # upon clicking, calls register function
 
-        self.main_screen.mainloop()
+        self.root.mainloop()
+    def __main_screen(self):
+        self.__main_screen = Toplevel(self.root)  # create window
+        self.__main_screen.geometry("300x250")  # set window size
+        self.__main_screen.title("Account selection")  # set window title
+
+        Label(self.__main_screen, text="Please Select Your Choice", bg="white", width="20", height="2").pack()  # pack puts it into screen
+        Label(self.__main_screen,text="").pack()  # leaves a gap for aesthetic purposes
+
+        Button(self.__main_screen,text="Login", height="2", width="15", command=self.__login).pack()  # upon clicking, calls login function
+        Label(self.__main_screen,text="").pack()
+
+        Button(self.__main_screen,text="Register", height="2", width="15",
+               command=self.__register).pack()  # upon clicking, calls register function
+        Label(self.__main_screen,text="").pack()
+
+        Button(self.__main_screen,text="Continue as Guest", height="2", width="15", command=self.__continue).pack()
+
+
 
     def __register(self):
-        self.register_screen = Toplevel(self.main_screen)
+        self.register_screen = Toplevel(self.__main_screen) # like a stack
         self.register_screen.title("Register")
         self.register_screen.geometry("300x250")
 
@@ -119,14 +147,14 @@ class logreg:
         self.password_entry.pack()
 
         Label(self.register_screen, text="").pack()
-        Button(self.register_screen, text="Register", width=10, height=1, bg="blue", command=self.register_user).pack()
+        Button(self.register_screen, text="Register", width=10, height=1, bg="blue", command=self.__register_user).pack()
 
-    def register_user(self):
+    def __register_user(self):
         self.db.insert(self.username.get(), self.password.get())
         print('You have been registered')
 
     def __login(self):
-        self.login_screen = Toplevel(self.main_screen)
+        self.login_screen = Toplevel(self.__main_screen)
         self.login_screen.title("Login")
         self.login_screen.geometry("300x250")
 
@@ -168,6 +196,8 @@ class logreg:
             Label(self.login_failed_screen, text="Invalid credentials supplied").pack()  # will not say which of user
             # and password are wrong, more secure
             Button(self.login_failed_screen, text="OK", command=delete_login_failed_screen).pack()
+            self.login_success = False
+
 
         def login_verify():
             username = self.username_to_verify.get()
@@ -175,14 +205,45 @@ class logreg:
 
             if self.db.user_password_match(username, password):
                 login_success()
+                self.login_success = True
+
             else:
                 login_failed()
+                self.login_success = False
 
         Label(self.login_screen, text="").pack()
         Button(self.login_screen, text="Confirm Login", width=10, height=1, command=login_verify).pack()
 
-    # db = database()
+    def __continue(self):
+        self.__continue_screen = Toplevel(self.__main_screen)  # like a stack
+        self.__continue_screen.title("Welcome!")
+        self.__continue_screen.geometry("500x350")
+
+        Label(self.__continue_screen, text="Welcome to the handwriting game - digit edition! \n In this game you will be tested on how neatly you can write numbers.\n The computer will read your writing, \n and will give you an overall score for how legible your writing was!\n Your score will be compared with your friends to see \nwhich of you has the neater writing!").pack()
+        Label(self.__continue_screen, text="").pack()
+        Label(self.__continue_screen, text="").pack()
+        Label(self.__continue_screen, text="").pack()
+
+        Button(self.__continue_screen, text='Get started!', width=10, height=1, command=self.__main_screen.quit).pack()
+
+    def __leaderboard(self):
+        self.__leaderboard_screen = Toplevel(self.root)  # like a stack
+        self.__leaderboard_screen.title("Leaderboard")
+        self.__leaderboard_screen.geometry("300x250")
+
+        leaders = self.db.display_leaderboard()
+        print('hello')
+        print(leaders)
+        row1 = 'Username,accuracy, number of attempts'
+        for row in leaders:
+            print(row)
+            Label(self.__leaderboard_screen, text=row1).pack()
+            Label(self.__leaderboard_screen, text=row).pack()
+        #Label(self.__leaderboard_screen, text='row').pack()
 
 
 l = logreg()
-l.main_screen()
+l._start_screen()
+
+# import main
+# main.predict(l.login_success)
