7af75bde778ae0d62c85b623667a94641030bf6f
nickan2c <63848807+nickan2c@users.noreply.github.com>
2020-12-18 18:49:26 +0000

    December submission

--- draw_number.py
+++ draw_number.py
@@ -1,15 +1,9 @@
 # https://www.python-course.eu/tkinter_events_binds.php
-import os
-
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
 from tkinter import *
 from PIL import Image, ImageFilter, EpsImagePlugin
 import tensorflow as tf
 import numpy as np
-import matplotlib.pyplot as plt
 import random
-import sqlite3
 
 
 class drawing:
@@ -40,23 +34,16 @@ class drawing:
 
     def submit(self):
         self.submitted = True
-        self.canvas.postscript(file='drawing.eps')  #
-        # Used https://stackoverflow.com/questions/9886274/how-can-i-convert-canvas-content-to-an-image
-
-        # I was able to get the contents of the canvas, by using the built in ‘canvas.postscript’ method of tkinter,
-        # which returns the canvas object as an image. Then I am able to use the module PIL  to convert that into a
-        # png file, which I can save.
+        self.canvas.postscript(file='drawing.eps')  
 
         EpsImagePlugin.gs_windows_binary = r'C:\Program Files\gs\gs9.53.3\bin\gswin64c'  # this line was used in order to
         # fix an error I was having, where the ghostscript file used in Image.open wasn't found
-        # https://stackoverflow.com/questions/44587376/oserror-unable-to-locate-ghostscript-on-paths
         
         img = Image.open("drawing.eps")
-        img.save(f"drawing{self.attempt_number}.png", "png")  # saves file using f string. ie it will be drawing1,
-        # drawing2, etc. This way i can retrieve the images later, as before it would just save to drawing.png each time
+        img.save(f"drawing{self.attempt_number}.png", "png")  
 
-        if self.attempt_number == 0:  # if windows have been destroyed, it gives an error that they cannot be
-            # destroyed since they already have been. By doing this they destroy only once
+        if self.attempt_number == 0:  # if windows have been destroyed, it would give an error that they cannot be
+            # destroyed, so this only destroys on first round
             self.window.destroy()
 
         self.root.destroy()
@@ -103,23 +90,38 @@ def prepare_img(image):
 loaded_model = tf.keras.models.load_model('cnn2.h5')
 
 
+class queue:  # used to select a prompt, and make sure the prompt isn't the same twice in a row
+    def __init__(self):
+        self.nums = []
+        for i in range(1, 10):
+            self.nums.append(i)
+        random.shuffle(self.nums)  # shuffles nums
+
+    def refresh(self):
+        self.last = self.dequeue()
+        self.enqueue(self.last)
+        return self.last
+
+    def enqueue(self, item):
+        self.nums.insert(0, item)  # puts it a front. Didn't use append since pop returns final element of list,
+        # so adding to queue from the front makes it easier
+
+    def dequeue(self):
+        return self.nums.pop()
+
+
 def predict(window, num_games=1, logged_in=False, ):  # the tkinter window is passed into here, so it can be closed
     predictions = {}
     f = open('user_score.txt', 'w')
     f.write('')  # to clear the file, since a new user is now playing, so it will need to be cleared
     f.close()
 
-    prompts = []
-    for i in range(1, 10):
-        prompts.append(i)
+    prompts = queue()
 
     for game_num in range(0, num_games):  # how many times game will be played
-        prompt = random.randint(0, 9)  # use list and remove from list so that not same number each time. if empty then
-        # can remake list
+        prompt = prompts.refresh()
 
         d = drawing(prompt, game_num, window)
-        # passes in prompt ie what the user will draw and how many times they have drawn,
-        # and window so that it can destroy it later so that it can move on with the program
 
         x = np.array(prepare_img(f'drawing{game_num}.png'))
         y = np.reshape(x, (28, 28))
@@ -136,25 +138,18 @@ def predict(window, num_games=1, logged_in=False, ):  # the tkinter window is pa
         print(prediction)
 
         guess = np.argmax(prediction)  # gets largest value from guesses, returns index
-        perc_certain = round(max(prediction[0]) * 100, 2)  # rounds it to 2 decimal places
+        perc_certain = int(round(max(prediction[0]) * 100))
 
-        predictions[str(game_num + 1)] = str(prompt) + str(guess) + ' ' + str(perc_certain)
+        predictions[str(game_num + 1)] = str(prompt) + ' ' + str(guess) + ' ' + str(perc_certain)
         # adds to dictionary their attempt number(key),prompt, network guess, networks certainty
 
         print(f"I'm {round(max(prediction[0]) * 100, 2)}% sure that that's a {np.argmax(prediction)} ")
+        # rounds it to 2 decimal places
 
-    if not logged_in:
-        f = open('user_score.txt', 'a')  # w because rewrite this file for each user. This is just temporary
-        for item in predictions.items():
-            line = ' '.join(item) + '\n'  # key + value assigned to that key in dictionary, ie number and
-            # percentage certainty
-            print(line)
-            f.write(line)
-        f.close()
-        # now the score is written to text file, read it.
-
-    print(predictions)
-
-
-if __name__ == '__main__':  # only runs if i am running the file, wont run if imported
-    predict()
+    f = open('user_score.txt', 'a')  # w because rewrite this file for each user. This is just temporary
+    for item in predictions.items():
+        line = ' '.join(item) + '\n'  # key + value assigned to that key in dictionary, ie number and
+        # percentage certainty
+        # print(line)
+        f.write(line)
+    f.close()
