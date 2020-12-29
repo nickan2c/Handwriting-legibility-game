416ffe82b313209efd78da65e197b3a2a12ef79b
nickan2c <63848807+nickan2c@users.noreply.github.com>
2020-12-29 22:12:59 +0000

    loading in data

--- Neural_net.py
+++ Neural_net.py
@@ -1,30 +1,15 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
 import numpy as np
 
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()  # loading in database using tensorflow
+x_train, x_test = x_train / 255.0, x_test / 255.0
 
-class Network:
-
-    def __init__(self, sizes):  # sizes is the amount of neurons in each layer, in the form [x,y,z, ...]
-        self.num_layers = len(sizes)
-        self.sizes = sizes
-        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]  # numpy built in function outputs a random number,from
-        # a Gaussian distribution, with mean 0 and standard deviation 1
-        # values passed in represent size of vector, eg np.random.randn(3,1) will give a vector [[x],[x],[x]]
-        # it is set to 1 because each bias is only for one neuron
-        self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]  # zip concatenates each value at
-        # the same point in two arrays   eg zip([1,3,5], [2,4,6]) would give 1 2, 3 4, 5 6
-        # creates a numpy array representing each weighted connection between each layer
-
-    def feedforward(self, inputs): # output of a neuron is w * inputs + b
-        for b, w in zip(self.biases, self.weights):
-            a = np.dot(w, inputs) + b # np.dot is a multiplication of vectors, since * does not work in this case
-            z = sigmoid(a)  # applying sigmoid
-        return z
+# print(x_train[0], x_train[0].shape)
+# plt.imshow(x_train[5], cmap='gray')
+# plt.show()
 
 
-def sigmoid(x):
-    # applying the sigmoid activation function
-    return 1 / (1 + np.exp(-x))  # e^-x
 
 
-network = Network([2, 3, 1])
-print(network.weights)
