99f6cf997a7a97f523b37950f65d873778d6ec8d
nickan2c <63848807+nickan2c@users.noreply.github.com>
2020-12-30 11:48:40 +0000

    Update Neural_net.py

--- Neural_net.py
+++ Neural_net.py
@@ -3,13 +3,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 mnist = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()  # loading in database using tensorflow
-x_train, x_test = x_train / 255.0, x_test / 255.0
+(training_data, training_labels), (test_data, test_labels) = mnist.load_data()  # loads in the mnist dataset
+training_data, test_data = training_data / 255, test_data / 255  # normalises mnist colours to between 0 and 1
 
-# print(x_train[0], x_train[0].shape)
-# plt.imshow(x_train[5], cmap='gray')
-# plt.show()
-
-
+print(training_data[0], training_data[0].shape)
+plt.imshow(training_data[5], cmap='gray')
+plt.show()
 
 
