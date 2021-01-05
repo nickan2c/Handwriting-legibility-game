3a4f0947c8a5876b33a575cf196276e6c8fe0996
nickan2c <63848807+nickan2c@users.noreply.github.com>
2021-01-05 11:45:28 +0000

    Update Neural_net.py

--- Neural_net.py
+++ Neural_net.py
@@ -26,7 +26,7 @@ def cross_entropy_loss(y, y_hat):
     :param y_hat: output of network
     :return: loss
     '''
-    L_sum = np.sum(np.multiply(y, np.log(y_hat))) # no subtract?
+    L_sum = np.sum(np.multiply(y, np.log(y_hat)))  # no subtract?
     m = y.shape[1]
     loss = -(1 / m) * L_sum
 
@@ -34,6 +34,10 @@ def cross_entropy_loss(y, y_hat):
 
 
 def load_dataset():
+    '''
+    loads dataset using tensorflow
+    :return: training data, test data, training labels, test labels
+    '''
     mnist = tf.keras.datasets.mnist  # loading with tensorflow
     (training_data, training_labels), (test_data, test_labels) = mnist.load_data()  # loads in the mnist dataset
     training_data, test_data = training_data / 255, test_data / 255  # normalises mnist colours to between 0 and 1
@@ -62,64 +66,66 @@ def get_one_hot(targets, nb_classes=10):
 
 
 class NeuralNetwork:
-    def __init__(self, x_train, y_train):
-        self.y_train = y_train
+    def __init__(self, y_train):
+        self.y_train = y_train.T
         self.learning_rate = 0.1
-        self.num_images = 60000  # num images
+        self.num_train_images = 60000  # num images
 
+        # initialise weights and biases in shape of layers
         self.W1 = np.random.randn(256, 784)
         self.b1 = np.zeros((256, 1))
         self.W2 = np.random.randn(10, 256)
         self.b2 = np.zeros((10, 1))
 
     def forward_pass(self, inputs):
+        # output of each layer stored so that network can use it in backprop
+
         self.Z1 = np.matmul(self.W1, inputs) + self.b1
         self.l1_output = sigmoid(self.Z1)
         self.Z2 = np.matmul(self.W2, self.l1_output) + self.b2
         self.final_output = softmax(self.Z2)
 
-        return self.final_output
+        return self.final_output  # value is returned for viewing output
 
-    def backprop(self, targets, inputs):
-        # layer 2
-        d_cost = self.final_output - targets
-        d_W2_d_cost = (1. / self.num_images) * np.matmul(d_cost, self.l1_output.T)
-        d_b2_d_cost = (1. / self.num_images) * np.sum(d_cost, axis=1, keepdims=True)
+    def backprop(self, inputs):
+        # finding derivative of cost with respect to weights/biases
+        d_cost = self.final_output - self.y_train
+        d_cost_d_W2 = (1. / self.num_train_images) * np.matmul(d_cost, self.l1_output.T)
+        d_cost_d_b2 = (1. / self.num_train_images) * np.sum(d_cost, axis=1, keepdims=True)
 
         d_l1_output = np.matmul(self.W2.T, d_cost)
         dZ1 = d_l1_output * sigmoid(self.Z1, derivative=True) * (1 - sigmoid(self.Z1, derivative=True))
 
-        d_W1_d_cost = (1. / self.num_images) * np.matmul(dZ1, inputs.T)
-        d_b1_d_cost = (1. / self.num_images) * np.sum(dZ1, axis=1, keepdims=True)
+        d_cost_d_W1 = (1. / self.num_train_images) * np.matmul(dZ1, inputs.T)
+        d_cost_d_b1 = (1. / self.num_train_images) * np.sum(dZ1, axis=1, keepdims=True)
 
-        return d_W2_d_cost, d_b2_d_cost, d_W1_d_cost, d_b1_d_cost
+        return d_cost_d_W2, d_cost_d_b2, d_cost_d_W1, d_cost_d_b1
 
-    def update_parameters(self, d_W2_d_cost, d_b2_d_cost, d_W1_d_cost, d_b1_d_cost):
-        '''
-        Stochastic gradient descent used to update parameters
-        '''
-        self.W2 = self.W2 - self.learning_rate * d_W2_d_cost
-        self.b2 = self.b2 - self.learning_rate * d_b2_d_cost
-        self.W1 = self.W1 - self.learning_rate * d_W1_d_cost
-        self.b1 = self.b1 - self.learning_rate * d_b1_d_cost
+    def update_parameters(self, d_cost_d_W2, d_cost_d_b2, d_cost_d_W1, d_cost_d_b1):
+        # Stochastic gradient descent optimiser method used to update parameters
 
+        self.W2 = self.W2 - self.learning_rate * d_cost_d_W2
+        self.b2 = self.b2 - self.learning_rate * d_cost_d_b2
+        self.W1 = self.W1 - self.learning_rate * d_cost_d_W1
+        self.b1 = self.b1 - self.learning_rate * d_cost_d_b1
 
-    def train(self,inputs):
+    def train(self, inputs):
 
         for i in range(1000):
+            self.forward_pass(inputs)  # get current outputs 
+            # backpropagate to find derivatives and error
+            d_cost_d_W2, d_cost_d_b2, d_cost_d_W1, d_cost_d_b1 = self.backprop(x_train)
 
-            self.forward_pass(inputs)
-            d_W2_d_cost, d_b2_d_cost, d_W1_d_cost, d_b1_d_cost = self.backprop(y_train, x_train)
+            # update parameters
+            self.update_parameters(d_cost_d_W2, d_cost_d_b2, d_cost_d_W1, d_cost_d_b1)
 
-            self.update_parameters(d_W2_d_cost, d_b2_d_cost, d_W1_d_cost, d_b1_d_cost)
-
-            if i % 100 == 0:
+            if i % 100 == 0:  # print training cost every 100 epochs.
                 cost = cross_entropy_loss(self.y_train, self.final_output)
                 print(f'Epoch {i},cost:{cost}')
 
 
 x_train, x_test, y_test, y_train = load_dataset()
-x_train, x_test = x_train.T, x_test.T  # maybe not T
+x_train, x_test = x_train.T, x_test.T  
 
-nn = NeuralNetwork(x_train, y_train)
+nn = NeuralNetwork(y_train)
 nn.train(x_train)
