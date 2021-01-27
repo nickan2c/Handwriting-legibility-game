3f419502241a6eee1a1cd405882071d3f03a42cc
nickan2c <63848807+nickan2c@users.noreply.github.com>
2021-01-27 12:22:34 +0000

    Update server.py

--- server.py
+++ server.py
@@ -6,8 +6,9 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image, ImageFilter
 import my_nn
+import random
 
-server = "192.168.1.103"
+server = "192.168.1.114"
 port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -21,8 +22,6 @@ s.listen(2)
 print("Waiting for a connection, Server Started")
 
 
-# loaded_model = tf.keras.models.load_model('cnn2.h5')
-
 class drawing:  # have to redefine class here so that it can unpickle it using the class.
     def __init__(self, prompt, game_num, name='drawing0.png'):
         self.prompt = prompt
@@ -32,10 +31,6 @@ class drawing:  # have to redefine class here so that it can unpickle it using t
         self.certainty = 0
         self.mnist = np.array(self.prepare(name))
         # print(self.mnist)
-
-    def eval(self, correct, certainty):
-        self.correct = correct
-        self.certainty = certainty
 
     def prepare(self, image):
         im = Image.open(image).convert('L')  # opens the image in greyscale format, black and white
@@ -57,7 +52,7 @@ class drawing:  # have to redefine class here so that it can unpickle it using t
 
 
 def predict(x):
-    loaded_model = tf.keras.models.load_model('cnn2.h5')
+    # loaded_model = tf.keras.models.load_model('cnn2.h5')
     nn = my_nn.NeuralNetwork(load=True)
     prediction = nn.predict(x)
     guess = np.argmax(prediction)  # finds largest value from guesses, returns index
@@ -66,37 +61,81 @@ def predict(x):
     return perc_certain, guess
 
 
+class queue:  # used to select a prompt, and make sure the prompt isn't the same twice in a row
+    def __init__(self, length):
+        self.nums = []
+        for i in range(1, length):
+            self.nums.append(i)
+        random.shuffle(self.nums)  # shuffles nums
+
+    def refresh(self):
+        self.last = self.dequeue()
+        self.enqueue(self.last)
+        return self.last
+
+    def enqueue(self, item):
+        self.nums.insert(0, item)  # puts it a front. Didn't use append since pop returns final element of list,
+        # so adding to queue from the front makes it easier
+
+    def dequeue(self):
+        return self.nums.pop()
+
+
+prompts = queue(10)
+users = []
+num_games = 0
+selected_games = False
+
+
 def threaded_client(conn):
-    drawings = []
+    global num_games
+    global selected_games
+    global prompts
     conn.send(pickle.dumps('Connected to NN'))
-
-    reply = ""
+    reply = ''
     while True:
-        try:
-            data = pickle.loads(conn.recv(2048 * 4))
+        data = pickle.loads(conn.recv(2048 * 4))  # tuple containing data and instruction.
 
-            if not data:
-                print("Disconnected")
-                break
-            else:
-                reply = predict(data.mnist)
-
-                print("Received: ", data)
-                print("Sending : ", reply)
-
-            conn.sendall(pickle.dumps(reply))
-
-        except:
+        if not data:
+            print("Disconnected")
             break
+        else:
+            if data[1] == 'p':
+                reply = predict(data[0].mnist)
+
+            elif data[1] == 'init':
+                print('Waiting for friend')
+                users.append(data[0])
+
+            if data[1] == 'game':
+                print(users)
+                reply = users
+
+            if data[1] == 'num_games':
+                num_games = data[0]
+                selected_games = True
+
+            if data[1] == 'num_games?':
+                reply = num_games
+
+            if data[1] == 'selected?':
+                if selected_games == True:
+                    reply = 'yes'
+
+            if data[1] == 'prompts?':
+                reply = prompts
+            # print("Received: ", data)
+            # print("Sending : ", reply)
+
+        conn.sendall(pickle.dumps(reply))
 
     print("Lost connection")
     conn.close()
 
 
 currentPlayer = 0
+
 while True:
     conn, addr = s.accept()
     print("Connected to:", addr)
-
     start_new_thread(threaded_client, (conn,))
-    currentPlayer += 1
