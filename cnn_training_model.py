dc12d8d720040e437083b9dd0ca1d4b1329f90f7
nickan2c <63848807+nickan2c@users.noreply.github.com>
2021-02-07 11:25:42 +0000

    Update cnn_training_model.py

--- cnn_training_model.py
+++ cnn_training_model.py
@@ -1,19 +1,13 @@
 import numpy as np
 import os
-
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
 import tensorflow as tf
-import tensorflow.keras.layers as KL
-import tensorflow.keras.models as KM
-
 ## Dataset
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 x_train = np.expand_dims(x_train, axis=-1)  # it was 28,28, which cannot be inputted into a conv network,
-x_test = np.expand_dims(x_test, axis=-1)  # So I used numpy expand dimension, which expands it to 28,28,1
+x_test = np.expand_dims(x_test, axis=-1)  # So I used numpy expand dimension, which expands it to 1,28,28,1
 
 ## Model
 model = tf.keras.models.Sequential()
@@ -26,11 +20,11 @@ model.add(tf.keras.layers.Flatten())
 model.add(tf.keras.layers.Dense(10, activation='softmax'))
 
 model.summary()
-model.compile(optimizer="adam",
+model.compile(optimizer="SGD",
               loss="sparse_categorical_crossentropy",
               metrics=["accuracy"])
 
 model.fit(x_train, y_train, epochs=5)
 test_loss, test_acc = model.evaluate(x_test, y_test)
 print("Test Loss: {0} - Test Acc: {1}".format(test_loss, test_acc))
-model.save('cnn2.h5')
+model.save('cnn.h5')
