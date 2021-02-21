875af3cb0643a9fa275dadc361f1b69a6c78aa42
nickan2c <63848807+nickan2c@users.noreply.github.com>
2021-02-21 23:45:12 +0000

    Update training_model.py

--- training_model.py
+++ training_model.py
@@ -11,25 +11,17 @@ model = tf.keras.Sequential([
     tf.keras.layers.Flatten(input_shape=(784,)),
     tf.keras.layers.Dense(256, activation=tf.nn.relu),
     tf.keras.layers.Dense(256, activation=tf.nn.relu),
-    tf.keras.layers.Dense(256, activation=tf.nn.relu),
     tf.keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
 # creates model. I talk about my decisions for network size/layers in my documents
 
-model.compile(optimizer="adam",
+model.compile(optimizer="SGD",
                 loss='sparse_categorical_crossentropy',
                 metrics=["accuracy"])
 # select optimizer and loss function. Set it to display accuracy while training. More about this in the documentation
 
-model.fit(flattened_training_data, training_labels, epochs=10) # trains the model on the data
+model.fit(flattened_training_data, training_labels, epochs=5) # trains the model on the data
 
 model.evaluate(flattened_test_data, test_labels) # tests the model on unsees data to get an accuracy
-#
-# predictions = model.predict(test_data)
-# print(test_labels[0], predictions[0])
 
 model.save('trained_model.h5')  # saves structure of model, weights, training configuration
-#
-# loaded_model = tf.keras.models.load_model('trained_model.h5')
-# pred = model.predict(flattened_test_data)
-# print(np.argmax(pred[0]))
